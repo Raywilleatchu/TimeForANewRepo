bc33d57c0540521b710dc88883742527b4c4d32a
email <raymondmansour@quickenloans.com>
2021-09-28 00:50:02 -0400

    CardAPI

---
+++ Week12/CardsAPI/Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using CardsAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Xrm.Sdk.Messages;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CardsAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        public async Task<IActionResult> GenerateDeck()
+        {
+            string domain = "https://deckofcardsapi.com/";
+            string path = "api/deck/new/shuffle/?deck_count=1";
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(domain);
+            var connection = await client.GetAsync(path);
+            Root result = await connection.Content.ReadAsAsync<Root>();
+            return View(result);
+        }
+
+        public async Task<IActionResult> DrawCards(string id, int count)
+        {
+            string domain = "https://deckofcardsapi.com/";
+            string path = $"api/deck/{id}/draw/?count={count}";
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(domain);
+            var connection = await client.GetAsync(path);
+            Deck result = await connection.Content.ReadAsAsync<Deck>();
+            return View(result);
+        }
+
+        public IActionResult TakeCards(Deck deck)
+        {
+            deck.hand = deck.cards;
+            deck.showHand = true;
+            return Redirect("/Home/DrawCards");
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Week12/CardsAPI/Models/DeckOfCards.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CardsAPI.Models
+{
+    public class Root
+    {
+        public bool success { get; set; }
+        public string deck_id { get; set; }
+        public int remaining { get; set; }
+        public bool shuffled { get; set; }
+    }
+
+
+    public class Deck
+    {
+        public bool success { get; set; }
+        public string deck_id { get; set; }
+        public List<Card> cards { get; set; }
+        public int remaining { get; set; }
+        public List<Card> hand { get; set; }
+        public bool takeCard = false;
+        public bool showHand = false;
+    }
+
+    public class Card
+    {
+        public string code { get; set; }
+        public string image { get; set; }
+        public string value { get; set; }
+        public string suit { get; set; }
+    }
+}
